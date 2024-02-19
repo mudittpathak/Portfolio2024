d769bbdba9ccf7647099e4bd2bbfaba75c0fb2e3
mudittpathak <mudit.pathak@edu.sait.ca>
2024-02-18 18:50:10 -0700

    Initial commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,32 +1,48 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
-import Image from 'next/image';
-import styles from '../styles/Home.module.css';
 import Header from '../Components/Header';
+import Hero from '../Components/Hero';  
+import About from '../Components/About';
+import WorkExperience from '../Components/WorkExperience';
+import Skills from '../Components/Skills';
+import Projects from '../Components/Projects';
+import ContactMe from '../Components/ContactMe';
 
 const Home: NextPage = () => {
   return (
-   <div>
+   <div className="bg-[rgb(36,36,36)] text-white h-screen snap-y
+   snap-mandatory overflow-y-scroll overflow-x-hidden z-0 scrollbar scrollbar-track-gray-400/20
+   scrollbar-thumb-[#F7AB0A]/80">
     <Head>
       <title>Mudit's Portfolio</title>
     </Head>
+    
+    <Header />
 
-    {/*HEADER*/}
-    <Head
-    {/*HERO*/}
+    <section id="hero" className='snap-start'>
+      <Hero />
+    </section>
 
-    {/*ABOUT*/}
+    <section id='about' className='snap-center'>
+      <About />
+    </section>
 
-    {/*EXPERIENCE*/}
+    <section id="experience" className="snap-center">
+      <WorkExperience />
+    </section>
 
-    {/*SKILLS*/}
+    <section id="skills" className="snap-start">
+      <Skills />
+    </section>
 
-    {/*PROJECTS*/}
+    <section id="projects" className="snap-start">
+      <Projects />
+    </section> 
 
-    {/*CONTACT ME*/}
-
-    <h1 className="p-10 text-red-500">AWESOME PORTFOLIO</h1>
-   </div>
+    <section id='contact' className="snap-start">
+      <ContactMe /> 
+    </section>
+      </div>
   );
 }
 
